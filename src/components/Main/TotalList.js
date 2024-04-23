35f37c726f77fc5702902e033924feb24140bdff
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-23 11:27:25 +0900

    feat: 카테고리 렌더링

--- src/components/Main/TotalList.js
+++ src/components/Main/TotalList.js
@@ -4,11 +4,25 @@ const PressWrap = styled.div`
   width: 930px;
   height: 388px;
   border: 1px solid rgba(210, 218, 224, 1);
+`;
 
-  & nav {
-    height: 40px;
-    border-bottom: 1px solid rgba(210, 218, 224, 1);
-    background-color: rgba(245, 247, 249, 1);
+const Category = styled.nav`
+  height: 40px;
+  border-bottom: 1px solid rgba(210, 218, 224, 1);
+  background-color: rgba(245, 247, 249, 1);
+  display: flex;
+  align-items: center;
+
+  & div {
+    padding: 0px 16px;
+    font-size: 14px;
+    font-weight: 500;
+    color: rgba(135, 146, 152, 1);
+    text-decoration: none;
+  }
+
+  & div:hover {
+    cursor: pointer;
   }
 `;
 
@@ -52,7 +66,7 @@ const NewsList = styled.div`
   align-items: center;
   width: 882px;
   height: 260px;
-  margin: 0px 16px;
+  margin: 0px 30px;
 `;
 
 const Left = styled.div`
@@ -65,29 +79,121 @@ const Left = styled.div`
     height: 200px;
     margin-bottom: 10px;
   }
+
+  & img {
+    width: 320px;
+    height: 200px;
+    object-fit: cover;
+  }
+
+  & a {
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px;
+    color: rgba(20, 33, 43, 1);
+    text-decoration: none;
+  }
+`;
+
+const Right = styled.div`
+  width: 530px;
+
+  & ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  & li {
+    margin-bottom: 0.625rem;
+    text-align: left;
+  }
+
+  & a {
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px;
+    color: rgba(75, 89, 102, 1);
+    text-decoration: none;
+  }
+
+  & a:hover {
+    text-decoration: underline;
+  }
+
+  & div {
+    font-weight: 500;
+    font-size: 14px;
+    color: rgba(135, 146, 152, 1);
+    text-align: left;
+  }
 `;
 
 function TotalList({ news }) {
   const firstNews = news[0];
+  const { logoImageSrc, pressName, editedTime, headline, sideNews } = firstNews;
+  const categories = [
+    "종합/경제",
+    "방송/통신",
+    "IT",
+    "영자지",
+    "스포츠/연예",
+    "매거진/전문지",
+    "지역",
+  ];
+
   return (
     <PressWrap>
-      <nav></nav>
+      <Category>
+        {categories.map((category) => (
+          <div>{category}</div>
+        ))}
+      </Category>
       <PressInfo>
         <span>
-          <img src={firstNews.logoImageSrc} alt={firstNews.pressName} />
+          <img src={logoImageSrc} alt={pressName} />
         </span>
-        <span className="edit-date">{firstNews.editedTime}</span>
+        <span className="edit-date">{editedTime}</span>
         <button>+ 구독하기</button>
       </PressInfo>
       <NewsList>
         <Left>
           <div>
-            <a href={firstNews.headline.href}></a>
+            <a href={headline.href}>
+              <img src={headline.thumbnailSrc} alt={headline.title} />
+            </a>
+            <a href={headline.href}>{headline.title}</a>
           </div>
         </Left>
+        <Right>
+          <ul>
+            {sideNews.map((news) => (
+              <li>
+                <a href={news.href}>{news.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div>{pressName}에서 직접 편집한 뉴스입니다.</div>
+        </Right>
       </NewsList>
     </PressWrap>
   );
 }
+
+// function groupByCategory(news) {
+//   const categoriesMap = new Map();
+
+//   news.forEach((item, index) => {
+//     const { category } = item;
+//     if (categoriesMap.has(category)) {
+//       const { firstIndex, count } = categoriesMap.get(category);
+//       categoriesMap.set(category, { firstIndex, count: count + 1 });
+//     } else {
+//       categoriesMap.set(category, { firstIndex: index, count: 1 });
+//     }
+//   });
+
+//   return categoriesMap;
+// }
 
 export default TotalList;
